50c8518d25343bce1fdac694bb29c431add80e7f
Ivo Mujo <ivo.mujo.3@gmail.com>
2020-12-24 05:55:24 +0100

    solving potential bugs

--- meta_edit.py
+++ meta_edit.py
@@ -20,32 +20,38 @@ def main():
         # remove extension
         song_no_extension = os.path.splitext(song)[0]
         # split name
-        song_name, artist_name, genre = [s.strip() for s in [splited.strip() for splited in song_no_extension.split('-')]]
+        if len(song.split('-')) == 3:
+            song_name, artist_name, genre = [s.strip() for s in [splited.strip() for splited in song_no_extension.split('-')]]
 
 
-        # load files
-        song_file = eyed3.load(os.path.join(songs_path, song))
-        image_file = open(os.path.join(images_path, artistToImageName(artist_name)), 'rb').read()
+            # load files
+            song_file = eyed3.load(os.path.join(songs_path, song))
+            image_file = open(os.path.join(images_path, artistToImageName(artist_name)), 'rb').read()
 
-        # set title
-        song_file.tag.title = song_name
+            if song_file.tag is not None:
+                # set title
+                song_file.tag.title = song_name
 
-        # set artist
-        song_file.tag.artist = artist_name
+                # set artist
+                song_file.tag.artist = artist_name
 
-        # set image
-        song_file.tag.images.set(1, image_file, 'image/jpg')
+                # set image
+                song_file.tag.images.set(1, image_file, 'image/jpg')
 
-        # set genre
-        song_file.tag.genre = genre
+                # set genre
+                song_file.tag.genre = genre
 
-        # save
-        song_file.tag.save()
+                # save
+                song_file.tag.save()
 
-        # rename
-        old_dir = os.path.join(songs_path, song)
-        new_dir = os.path.join(songs_path, '{} - {}.mp3'.format(song_name, artist_name))
-        os.rename(old_dir, new_dir)
+                # rename
+                old_dir = os.path.join(songs_path, song)
+                new_dir = os.path.join(songs_path, '{} - {}.mp3'.format(song_name, artist_name))
+                os.rename(old_dir, new_dir)
+            else:
+                print('Format error. Please go to song properties, add a random tag and restart the program. File: "{}"'.format(song))
+        else:
+            print('Wrong naming scheame. File: "{}"'.format(song))
 
 
 
